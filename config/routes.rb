c9c8d83180f93d7eba06d0335ffcf4ada67aa4b1
Roger Bazemore <rogerbazemore@gmail.com>
2023-04-11 16:38:31 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  
+  root 'friends#index'
+  get 'home/about'
+  
+  #root 'home#index'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
